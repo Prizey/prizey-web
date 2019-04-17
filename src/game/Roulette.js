ed1eda0a0436fb14361aa05d45eba3d29081d02e
Sergio Freitas <sergio.sgfreitas@gmail.com>
2019-04-17 16:15:39 -0300

    :camera_flash: update snapshot and tests

--- src/game/Roulette.js
+++ src/game/Roulette.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import useInterval from '@use-it/interval'
 import posed from 'react-pose'
+
+export const getItemSpeed = (price, speed, multiplier) => {
+  const PRICE_FACTOR = process.env.REACT_APP_PRICE_FACTOR || 1
+  const deltaSpeed = (PRICE_FACTOR / price) * multiplier * speed
+  return Math.max(deltaSpeed, 80)
+}
 
 const styles = () => ({
   img: {
@@ -17,18 +22,22 @@ const styles = () => ({
   },
 })
 
+export const enterTransition = ({ speed }) => ({
+  opacity: { duration: Math.min(speed - 50, 500), ease: 'easeIn' },
+  x: { duration: 10 },
+})
+
 const PosedImg = posed.img({
   enter: {
     opacity: 1,
-    transition: {
-      x: { duration: 1 },
-    },
+    transition: enterTransition,
     x: 0,
   },
   leave: {
     opacity: 0,
     transition: {
-      x: { duration: 1 },
+      opacity: { duration: 10, ease: 'easeOut' },
+      x: { duration: 10 },
     },
     x: -9999,
   },
@@ -36,22 +45,39 @@ const PosedImg = posed.img({
 
 export default withStyles(styles)(({ classes, speed, onSelectItem, data }) => {
   const [next, setNext] = useState(0)
+  const multiplier = 2
 
-  useInterval(() => {
-    setNext((next + 1) % data.length)
-  }, speed)
+  useLayoutEffect(() => {
+    const item = data[next]
+    const timeout = setTimeout(() => {
+      setNext((next + 1) % data.length)
+    }, getItemSpeed(item.price, speed, multiplier))
+
+    // console.log(
+    //   'price and velocity',
+    //   item.price,
+    //   getItemSpeed(item.price, speed, multiplier),
+    // )
+
+    return () => clearTimeout(timeout)
+  }, [speed, data, next, setNext])
 
   return (
     <div className={classes.root}>
       {data.map((item, id) => (
-        <PosedImg
+        <div
           key={`${item.id}-${id}`}
-          onClick={() => onSelectItem(item)}
-          pose={next === id ? 'enter' : 'leave'}
-          src={item.image}
-          alt={item.title}
-          className={classes.img}
-        />
+          testID={`rouletteItem-${id}`}
+          onMouseDown={() => onSelectItem(item)}
+        >
+          <PosedImg
+            pose={next === id ? 'enter' : 'leave'}
+            src={item.image}
+            alt={item.title}
+            className={classes.img}
+            speed={getItemSpeed(item.price, speed, multiplier)}
+          />
+        </div>
       ))}
     </div>
   )
