cf6fbaed80133ab86b9f7c6d175030e2f92c4286
Sergio Freitas <sergio.sgfreitas@gmail.com>
2019-04-29 15:21:40 -0300

    :ok_hand: adjust due to code review changes

--- src/game/ClaimProductScreen/ClaimProductScreen.js
+++ src/game/ClaimProductScreen/ClaimProductScreen.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from '@reach/router'
-import { withStyles } from '@material-ui/core/styles'
 import { Button, Typography } from '@material-ui/core'
 
 import { clearProduct } from 'store/basket/actions'
@@ -9,56 +8,40 @@ import { clearProduct } from 'store/basket/actions'
 import GoBack from 'design/GoBack/GoBack'
 import Layout from 'design/Layout/Layout'
 import Caption from 'design/Caption/Caption'
+import ProductImage from 'design/ProductImage'
 
 import SellItBack from './SellItBack'
 
-const styles = theme => ({
-  productImage: {
-    display: 'block',
-    margin: 'auto',
-    marginBottom: theme.spacing.md,
-    marginTop: theme.spacing.md,
-    width: '130px',
-  },
-})
+const InnerComponent = ({ product, classes, difficulty, navigate, ...props }) =>
+  product ? (
+    <Layout
+      leftIcon={<GoBack to="/" />}
+      caption={<Caption difficulty={difficulty} />}
+    >
+      <Typography align="center" variant="h5">
+        {product.title}
+      </Typography>
 
-const InnerComponent = withStyles(styles)(
-  ({ product, classes, difficulty, navigate, ...props }) =>
-    product ? (
-      <Layout
-        leftIcon={<GoBack to="/" />}
-        caption={<Caption difficulty={difficulty} />}
+      <ProductImage product={product} />
+      <Typography align="center">MSRP: ${product.price}</Typography>
+
+      <SellItBack amount={3} clearProduct={props.clearProduct} />
+
+      <Button
+        variant="contained"
+        color="primary"
+        fullWidth
+        aria-label="I want it"
+        onClick={() => {
+          navigate('/shipping-info')
+        }}
       >
-        <Typography align="center" variant="h5">
-          {product.title}
-        </Typography>
-
-        <img
-          alt={product.title}
-          src={product.image}
-          className={classes.productImage}
-        />
-
-        <Typography align="center">MSRP: ${product.price}</Typography>
-
-        <SellItBack amount={3} clearProduct={props.clearProduct} />
-
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          aria-label="I want it"
-          onClick={() => {
-            navigate('/shipping-info')
-          }}
-        >
-          I WANT IT!
-        </Button>
-      </Layout>
-    ) : (
-      <Redirect to="/" noThrow />
-    ),
-)
+        I WANT IT!
+      </Button>
+    </Layout>
+  ) : (
+    <Redirect to="/" noThrow />
+  )
 
 export const mapState = ({ basket = {} }) => ({
   product: basket.product,
--- src/shipping/ShippingConfirmation.js
+++ src/shipping/ShippingConfirmation.js
@@ -1,59 +1,46 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from '@reach/router'
-import { withStyles } from '@material-ui/core/styles'
 import { Button, Typography } from '@material-ui/core'
 
 import { clearProduct } from 'store/basket/actions'
 
 import GoBack from 'design/GoBack/GoBack'
 import Layout from 'design/Layout/Layout'
+import ProductImage from 'design/ProductImage'
 
-const styles = theme => ({
-  productImage: {
-    display: 'block',
-    margin: 'auto',
-    marginBottom: theme.spacing.md,
-    marginTop: theme.spacing.md,
-    width: '200px',
-  },
-})
+const ShippingComponent = ({
+  product,
+  classes,
+  difficulty,
+  navigate,
+  ...props
+}) =>
+  product ? (
+    <Layout leftIcon={<GoBack to="/" />}>
+      <Typography align="center" variant="h5">
+        Sweet!
+      </Typography>
 
-const ShippingComponent = withStyles(styles)(
-  ({ product, classes, difficulty, navigate, ...props }) =>
-    product ? (
-      <Layout leftIcon={<GoBack to="/" />}>
-        <Typography align="center" variant="h5">
-          Sweet!
-        </Typography>
+      <ProductImage product={product} style={{ width: '200px' }} />
+      <Typography align="center">We&apos;ll confirm through email.</Typography>
 
-        <img
-          alt={product.title}
-          src={product.image}
-          className={classes.productImage}
-        />
-
-        <Typography align="center">
-          We&apos;ll confirm through email.
-        </Typography>
-
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          aria-label="Play again"
-          onClick={() => {
-            props.clearProduct()
-            navigate('/')
-          }}
-        >
-          PLAY AGAIN
-        </Button>
-      </Layout>
-    ) : (
-      <Redirect to="/" noThrow />
-    ),
-)
+      <Button
+        variant="contained"
+        color="primary"
+        fullWidth
+        aria-label="Play again"
+        onClick={() => {
+          props.clearProduct()
+          navigate('/')
+        }}
+      >
+        PLAY AGAIN
+      </Button>
+    </Layout>
+  ) : (
+    <Redirect to="/" noThrow />
+  )
 
 export const mapState = ({ basket = {} }) => ({
   product: basket.product,
@@ -68,5 +55,5 @@ export default ({ currentUser, ...props }) =>
   currentUser ? (
     <ShippingConfirmation {...props} />
   ) : (
-    <Redirect to="/sign-in?next=/shipping-confirmation" noThrow />
+    <Redirect to="/sign-in" noThrow />
   )
