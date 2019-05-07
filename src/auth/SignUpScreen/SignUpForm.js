9837a9ab97799528675aaaefea95f2c6d9079b56
Rodrigo Avellar <roavellarm@gmail.com>
2019-05-07 01:25:20 -0300

    Add message to SignUpFrom.js

--- src/auth/SignUpScreen/SignUpForm.js
+++ src/auth/SignUpScreen/SignUpForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 import { SignUp } from 'seasoned-auth-forms-web'
 
 import SubmitButton from 'design/SubmitButton/SubmitButton'
@@ -11,11 +12,17 @@ const styles = theme => ({
     justifyContent: 'center',
     marginTop: theme.spacing.sm,
   },
+  message: {
+    marginTop: theme.spacing.xs,
+  },
 })
 
 export const FormBottom = withStyles(styles)(({ classes, ...props }) => (
   <React.Fragment>
     <SubmitButton {...props} label="REGISTER" />
+    <Typography align="center" variant="h6" className={classes.message}>
+      You confirm you&apos;re 18
+    </Typography>
     <div className={classes.links}>
       <SecondaryLink to="/sign-in" label="Already have an account?" />
     </div>
