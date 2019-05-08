8d1008dd05a629e7ce65435a705da27dde20b284
Rodrigo Avellar <roavellarm@gmail.com>
2019-05-08 19:14:38 -0300

    Add RegisterPageView.js

---
+++ src/design/Layout/RegisterPageView.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactGA from 'react-ga'
+
+export default class RegisterPageView extends React.Component {
+  render() {
+    return null
+  }
+
+  componentDidMount() {
+    const { location, currentUser } = this.props
+    const gaOptions = currentUser
+      ? {
+          gaOptions: {
+            userId: currentUser.id,
+          },
+        }
+      : {}
+
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_CODE, {
+      testMode: process.env.REACT_APP_DEBUG === '1',
+      debug: true,
+      ...gaOptions,
+    })
+
+    ReactGA.pageview(location.pathname)
+  }
+}
