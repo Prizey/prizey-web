46f4bcd5ac0e240454f03c66733beb928e29b3c3
Felipe Freitag Vargas <ffvargas@gmail.com>
2019-05-22 12:59:28 -0300

    Change product id to string

--- src/design/ConfirmLeaveDialog/ConfirmLeaveDialog.js
+++ src/design/ConfirmLeaveDialog/ConfirmLeaveDialog.js
@@ -3,6 +3,6 @@ import Dialog from 'design/Dialog/Dialog'
 
 export default props => (
   <Dialog title="Are you sure?" {...props}>
-    You are leaving the page and you will lose your tickets. Are you sure?
+    You are leaving the page and you will lose your diamonds. Are you sure?
   </Dialog>
 )
